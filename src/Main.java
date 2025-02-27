c9aac9bc0a9bd93e51e76e014ff1a2f1918a953f
KarenJCastillo <kc6539769@gmail.com>
2025-02-27 17:53:32 -0500

    adivinar

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,32 @@
 import java.sql.SQLOutput;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int max = 10;
-        int min = 1;
-        int rondas;
-        int dado1;
-        int dado2;
-        int contadordado1 =0;
-        int contadordado2 =0;
-
         Random ale = new Random();
-        rondas = ale.nextInt((max - min + 1)) + min;
-        for (int i = 1; i <= rondas; i++) {
-            dado1 = ale.nextInt((6 - 1 + 1)) + 1;
-            contadordado1+=dado1; //significa que tomara la variable de contador y lo sumara con los resultados del dado1
-            System.out.println("el dado uno es de : " + dado1);
-            dado2 = ale.nextInt((6 - 1 + 1)) + 1;
-            contadordado2 +=dado2;
-            System.out.println("el dado dos es de :" + dado2);
-            System.out.println("el numero de ronas generadas es de "+i);
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        int usuario;
+        int contador = 0;
+        numero = ale.nextInt((50 - 1 + 1)) + 1;
+        System.out.println(numero);
+        while (contador < 3) {
+            System.out.println("ingrese el numero ");
+            usuario = teclado.nextInt();
+            if (numero < usuario) {
+                System.out.println("tu numero es menor");
+            } else if (numero > usuario) {
+                System.out.println("tu numero es mayor ");
 
+            } else if (usuario == numero) {
+                System.out.println("ganaste");
+                break;
 
+            }
+            contador++;
+            System.out.println("perdiste");
         }
-        if (contadordado2<contadordado1){
-            System.out.println("el dado uno es el ganador con : "+contadordado1+"puntos de sumatoria");
-        }else {
-            System.out.println("el dado dos es el ganador con : "+contadordado2+"puntos de sumatoria ");
-        }
-
-
 
     }
 }
