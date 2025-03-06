27c18937bfcb02d9005382020d95374f3e894387
KarenJCastillo <kc6539769@gmail.com>
2025-03-05 21:42:51 -0500

    calculadora

--- src/Main.java
+++ src/Main.java
@@ -5,52 +5,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        Random ale = new Random();
-        double dinero;
-        int aleaotrio;
-        int jugador;
+        int num1;
+        int num2;
+        int signo;
 
-        aleaotrio = ale.nextInt((3 - 1 + 1)) + 1;
-        System.out.println("digite el dinero que quiere apostar");
-        dinero = teclado.nextDouble();
 
-        do {
-
-            if (aleaotrio == 1) {
-                System.out.println("se ha multiplicado su dinero");
-                dinero = dinero * 2;
-                System.out.println("su dinero es de:" + dinero);
-                System.out.println("quieres seguir? para aceptar ingrese 1 si no ingrese 2");
-                jugador = teclado.nextInt();
-                if (jugador == 1) {
-                    aleaotrio = ale.nextInt((3 - 1 + 1)) + 1;
-                    continue;
-                } else if (jugador == 0) {
-                    System.out.println("su dinero es de:" + dinero);
-                    break;
-                }
-                System.out.println("su dinero es de:" + dinero + "quiere seguir con el juego? para aceptar ingrese 1 si no ingrese 2");
-            } else if (aleaotrio == 2) {
-                System.out.println("se ha divido su dinero a la mitad");
-                dinero = dinero / 2;
-                System.out.println("su dinero es de:" + dinero);
-                System.out.println("quieres seguir? para aceptar ingrese 1 si no ingrese 2");
-                jugador = teclado.nextInt();
-                if (jugador == 1) {
-                    aleaotrio = ale.nextInt((3 - 1 + 1)) + 1;1
-
-                    continue;
-                } else if (jugador == 0) {
-                    System.out.println("su dinero es de:" + dinero);
-                    break;
-                }
-                System.out.println("su dinero es de:" + dinero + "quiere seguir con el juego?para aceptar ingrese 1 si no ingrese 2");
-            } else if (aleaotrio == 3) {
-                System.out.println("ha  perdido todo");
-                dinero = dinero - dinero;
-                System.out.println("su dinero es de:" + dinero);
+        while (true) {
+            System.out.println("digite el primer numero a operar");
+            num1 = teclado.nextInt();
+            System.out.println("digite el operador a utilizar escribiendo el numero al que es igual : + = 1, - = 2, * = 3, / = 4, ^ = 5, % = 6 , 7 = salir");
+            signo = teclado.nextInt();
+            if (signo == 7){
+                System.out.println("Esta terminando el proceso...");
+                break;
             }
-        } while (dinero > 0);
+            double resultado = switch (signo) {
+                case 1 -> {
+                    System.out.println("digite el segundo numero a operar");
+                    num2 = teclado.nextInt();
+                    yield num1 + num2;
+                }
+
+                case 2 -> {
+
+                    System.out.println("digite el segundo numero a operar");
+                    num2 = teclado.nextInt();
+                    yield num1 - num2;
+                }
+                case 3 -> {
+                    System.out.println("digite el segundo numero a operar");
+                    num2 = teclado.nextInt();
+                    yield num1 * num2;
+                }
+                case 4 -> {
+                    System.out.println("digite el segundo numero a operar");
+                    num2 = teclado.nextInt();
+                    yield num1 / num2;
+                }
+                case 5 -> Math.pow(num1, 2);
+                case 6 -> {
+                    System.out.println("digite el segundo numero a operar");
+                    num2 = teclado.nextInt();
+                    yield num1 % num2;
+                }
+                default -> 0.0;
+            };
+            System.out.println("resultado" + resultado);
+
+
+        }
 
     }
 
